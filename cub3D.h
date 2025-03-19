7d80971e9fee2214187d958900351f591bf630b8
axburin <burinaxel@gmail.com>
2025-03-19 21:39:43 +0100

    init

--- cub3D.h
+++ cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calberti <calberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axburin- <axburin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:55:35 by calberti          #+#    #+#             */
-/*   Updated: 2025/03/19 19:32:58 by calberti         ###   ########.fr       */
+/*   Updated: 2025/03/19 21:38:56 by axburin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,106 @@
 # include <errno.h>
 
 # define BUFFER_SIZE 50
+
+
+
+typedef struct s_player
+{
+	char	dir;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	int		has_moved;
+	int		move_x;
+	int		move_y;
+	int		rotate;
+}	t_player;
+
+
+typedef struct s_ray
+{
+	double	camera_x;
+	double	dir_x;
+	double	dir_y;
+	int		map_x;
+	int		map_y;
+	int		step_x;
+	int		step_y;
+	double	sidedist_x;
+	double	sidedist_y;
+	double	deltadist_x;
+	double	deltadist_y;
+	double	wall_dist;
+	double	wall_x;
+	int		side;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+}	t_ray;
+
+
+typedef struct s_mapinf
+{
+	int			fd;
+	int			line_count;
+	char		*path;
+	char		**file;
+	int			height;
+	int			width;
+	int			index_end_of_map;
+}	t_mapinf;
+
+typedef struct s_texinfo
+{
+	char			*north;
+	char			*south;
+	char			*west;
+	char			*east;
+	int				*floor;
+	int				*ceiling;
+	unsigned long	hex_floor;
+	unsigned long	hex_ceiling;
+	int				size;
+	int				index;
+	double			step;
+	double			pos;
+	int				x;
+	int				y;
+}	t_texinfo;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*win;
+	int			win_height;
+	int			win_width;
+	t_mapinf	mapinf;
+	char		**map;
+	t_player	player;
+	t_ray		ray;
+	int			**texture_pixels;
+	int			**textures;
+	t_texinfo	texinfo;
+	// t_img		minimap;
+}	t_data;
+
+
+
+
+typedef struct s_img
+{
+	void	*img;
+	int		*addr;
+	int		pixel_bits;
+	int		size_line;
+	int		endian;
+}	t_img;
+
+
+// parsing
 
 typedef struct s_texture
 {
@@ -103,5 +203,16 @@ int		normalize_map(t_config *config);
 int		find_player_position(t_config *config);
 int		check_map_borders(t_config *config);
 int		flood_fill(char **map, int x, int y, t_config *config);
+
+//init exec
+
+void	init_ray(t_ray *ray);
+void	init_player(t_player *player);
+
+// a revoir 
+void	init_map(t_mapinf *mapinfo);
+void	init_img_clean(t_img *img);
+void	init_data(t_data *data);
+
 
 #endif
---
+++ exec/init.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axburin- <axburin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 21:34:34 by axburin-          #+#    #+#             */
+/*   Updated: 2025/03/19 21:37:02 by axburin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3D.h"
+
+
+void	init_ray(t_ray *ray)
+{
+	ray->camera_x = 0;
+	ray->dir_x = 0;
+	ray->dir_y = 0;
+	ray->map_x = 0;
+	ray->map_y = 0;
+	ray->step_x = 0;
+	ray->step_y = 0;
+	ray->sidedist_x = 0;
+	ray->sidedist_y = 0;
+	ray->deltadist_x = 0;
+	ray->deltadist_y = 0;
+	ray->wall_dist = 0;
+	ray->wall_x = 0;
+	ray->side = 0;
+	ray->line_height = 0;
+	ray->draw_start = 0;
+	ray->draw_end = 0;
+}
+
+void	init_player(t_player *player)
+{
+	player->dir = '\0';
+	player->pos_x = 0.0;
+	player->pos_y = 0.0;
+	player->dir_x = 0.0;
+	player->dir_y = 0.0;
+	player->plane_x = 0.0;
+	player->plane_y = 0.0;
+	player->has_moved = 0;
+	player->move_x = 0;
+	player->move_y = 0;
+	player->rotate = 0;
+}
+
+void	init_data(t_data *data)
+{
+	data->mlx = NULL;
+	data->win = NULL;
+	data->win_height = 480;
+	data->win_width = 640;
+	init_player(&data->player);
+	// init_texture(&data->texinfo);
+	data->map = NULL;
+	init_map(&data->mapinf);
+	data->texture_pixels = NULL;
+	data->textures = NULL;
+}
+
+void	init_img_clean(t_img *img)
+{
+	img->img = NULL;
+	img->addr = NULL;
+	img->pixel_bits = 0;
+	img->size_line = 0;
+	img->endian = 0;
+}
+
+void	init_map(t_mapinf *mapinfo)
+{
+	mapinfo->fd = 0;
+	mapinfo->line_count = 0;
+	mapinfo->path = NULL;
+	mapinfo->file = NULL;
+	mapinfo->height = 0;
+	mapinfo->width = 0;
+	mapinfo->index_end_of_map = 0;
+}
